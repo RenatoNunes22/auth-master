a65a613e45b750f6174cb93c023ed73574a44fa3
RenatoNunes22 <renatonunes0011@hotmail.com>
2024-05-09 20:17:19 -0300

    feat: add shadcn-ui input

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,7 +1,27 @@
-"use-client";
-import CardWrapper from "./card-wrapper";
+"use client";
 
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { LoginSchema } from "@/schemas";
+import { Input } from "@/components/ui/input";
+import CardWrapper from "./card-wrapper";
+import {
+  Form,
+  FormField,
+  FormControl,
+  FormLabel,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
 const LoginForm = () => {
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
   return (
     <CardWrapper
       backButtonHref="./register"
@@ -9,7 +29,24 @@ const LoginForm = () => {
       showSocials={true}
       headerlabel="Welcome login form"
     >
-      <h1></h1>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(() => {})} className="space-y-6">
+          <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input {...field} type="email" />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+          </div>
+        </form>
+      </Form>
     </CardWrapper>
   );
 };
