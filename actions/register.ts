880d632ce36c53821e2bb8b9afdcb7a7226e5a00
RenatoNunes22 <renatonunes0011@hotmail.com>
2024-05-11 17:35:59 -0300

    feat: finished register page

--- actions/register.ts
+++ actions/register.ts
@@ -4,6 +4,7 @@ import * as z from "zod";
 import bcrypt from "bcrypt";
 import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas/index";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (data: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(data);
@@ -13,14 +14,8 @@ export const register = async (data: z.infer<typeof RegisterSchema>) => {
   }
 
   const { email, password, name } = validatedFields.data;
-
   const hashedPassword = await bcrypt.hash(password, 10);
-
-  const existingUser = await db.user.findUnique({
-    where: {
-      email,
-    },
-  });
+  const existingUser = await getUserByEmail(email);
 
   if (existingUser) {
     return { error: "Email already in use!" };
