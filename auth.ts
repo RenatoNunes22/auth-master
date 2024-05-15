72f814548cab3ab3243a1477d22a25db8114d35e
RenatoNunes22 <renatonunes0011@hotmail.com>
2024-05-14 21:09:18 -0300

    feat: added permission routes in the application

--- auth.ts
+++ auth.ts
@@ -1,7 +1,9 @@
 import NextAuth from 'next-auth'
+import { getUserById } from '@/data/user'
 import { PrismaAdapter } from '@auth/prisma-adapter'
 import { db } from '@/lib/db'
 import authConfig from './auth.config'
+import { UserRole } from '@prisma/client'
 
 export const {
     handlers: { GET, POST },
@@ -9,6 +11,24 @@ export const {
     signOut,
     auth,
 } = NextAuth({
+    callbacks: {
+        async session({ token, session }) {
+            if (token.sub && session.user) {
+                session.user.id = token.sub
+            }
+            if (token.role && session.user) {
+                session.user.role = token.role as UserRole
+            }
+            return session
+        },
+        async jwt({ token }) {
+            if (!token.sub) return token
+            const existUser = await getUserById(token.sub)
+            if (!existUser) return token
+            token.role = existUser.role
+            return token
+        },
+    },
     adapter: PrismaAdapter(db),
     session: { strategy: 'jwt' },
     ...authConfig,
