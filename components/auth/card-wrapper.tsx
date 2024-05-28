5f20d6ff3be1dffbaad17fcea9e89cfac5c027d3
RenatoNunes22 <renatonunes0011@hotmail.com>
2024-05-28 16:11:04 -0300

    fix: adjust back-button in card-wrapper

--- components/auth/card-wrapper.tsx
+++ components/auth/card-wrapper.tsx
@@ -1,40 +1,43 @@
-"use-client";
-import { Card, CardContent, CardFooter, CardHeader } from "../ui/card";
-import Header from "./header";
-import Social from "./social";
-import { BackButton } from "./back-button";
+'use-client'
+import { Card, CardContent, CardFooter, CardHeader } from '../ui/card'
+import Header from './header'
+import Social from './social'
+import { BackButton } from './back-button'
 
 interface CardWrapperProps {
-  children: React.ReactNode;
-  headerlabel: string;
-  backButtonLabel?: string;
-  backButtonHref?: string;
-  showSocials?: boolean;
+    children: React.ReactNode
+    headerlabel: string
+    backButtonLabel?: string
+    backButtonHref?: string
+    showSocials?: boolean
 }
 
 const CardWrapper = ({
-  children,
-  headerlabel,
-  backButtonLabel,
-  backButtonHref,
-  showSocials,
+    children,
+    headerlabel,
+    backButtonLabel,
+    backButtonHref,
+    showSocials,
 }: CardWrapperProps) => {
-  return (
-    <Card className="w-[400px] shadow-md">
-      <CardHeader>
-        <Header label={headerlabel} />
-      </CardHeader>
-      <CardContent>{children}</CardContent>
-      {showSocials && (
-        <CardFooter>
-          <Social />
-        </CardFooter>
-      )}
-      <CardFooter>
-        <BackButton label={backButtonLabel} href={backButtonHref} />
-      </CardFooter>
-    </Card>
-  );
-};
+    return (
+        <Card className="w-[400px] shadow-md">
+            <CardHeader>
+                <Header label={headerlabel} />
+            </CardHeader>
+            <CardContent>{children}</CardContent>
+            {showSocials && (
+                <CardFooter>
+                    <Social />
+                </CardFooter>
+            )}
+            <CardFooter>
+                <BackButton
+                    label={backButtonLabel ?? ''}
+                    href={backButtonHref ?? ''}
+                />
+            </CardFooter>
+        </Card>
+    )
+}
 
-export default CardWrapper;
+export default CardWrapper
