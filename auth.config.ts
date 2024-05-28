2805e35c63b4855816030f7f004d7a2bb4912f8c
RenatoNunes22 <renatonunes0011@hotmail.com>
2024-05-28 15:25:24 -0300

    feat: added error page

--- auth.config.ts
+++ auth.config.ts
@@ -2,11 +2,20 @@ import bcrypt from 'bcryptjs'
 import Credentials from 'next-auth/providers/credentials'
 import { LoginSchema } from '@/schemas'
 import { getUserByEmail } from './data/user'
-
+import GitHub from 'next-auth/providers/github'
+import Google from 'next-auth/providers/google'
 import type { NextAuthConfig } from 'next-auth'
 
 export default {
     providers: [
+        GitHub({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        }),
+        Google({
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        }),
         Credentials({
             async authorize(credentials) {
                 const validateFields = LoginSchema.safeParse(credentials)
