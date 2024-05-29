c40665901b41a50876c89b2988fadc12252e5a67
RenatoNunes22 <renatonunes0011@hotmail.com>
2024-05-29 13:45:21 -0300

    feat: reset password with email link

--- schemas/index.ts
+++ schemas/index.ts
@@ -5,6 +5,12 @@ export const LoginSchema = z.object({
   password: z.string().min(6),
 });
 
+export const NewPasswordSchema = z.object({
+  password: z.string().min(6, {
+    message: "Password must be at least 6 characters",
+  }),
+});
+
 export const ResetSchema = z.object({
   email: z.string().email({ message: "Invalid email" })
 });
